feaa3842bb808f2d5dd8546e801e6c877194cd84
Vasiliy Koleda <vk@elearningforce.com>
2018-09-07 15:59:13 +0300

    Full page layout support was added.

--- src/infrastructure/helper.ts
+++ src/infrastructure/helper.ts
@@ -1,6 +1,6 @@
 export class Helper {
     public static getProxyUrl(): string {
-        return 'https://assets-dev.365.systems';
-        //return 'https://assets-qa.365.systems';
+        //return 'https://assets-dev.365.systems';
+        return 'https://assets-qa.365.systems';
     }
 }
