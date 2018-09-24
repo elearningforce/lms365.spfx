82d0d7b7c05c71521a84b557e3c73bed28a56f90
Denis Bobrov <denis.bobrov@elearningforce.com>
2018-09-24 18:41:08 +0300

    Added hotfix build

--- src/infrastructure/helper.ts
+++ src/infrastructure/helper.ts
@@ -3,5 +3,6 @@ export class Helper {
         return 'https://assets.365.systems';
         //return 'https://assets-dev.365.systems';
         //return 'https://assets-qa.365.systems';
+        //return 'https://assets-hotfix.365.systems';
     }
 }
